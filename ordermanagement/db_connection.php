3f096e24be1a0ea9dd5e382c517a79726fc4854a
BuenafeIII <113277949+thirdbuenafe12@users.noreply.github.com>
2024-05-15 20:31:22 +0800

    Update db_connection.php

--- ordermanagement/db_connection.php
+++ ordermanagement/db_connection.php
@@ -1,14 +1,12 @@
 <?php
-// Database credentials
-$servername = "localhost"; // Change if different
-$username = "third"; // Change to your database username
-$password = "third12"; // Change to your database password
-$dbname = "db_php_sql"; // Change to your database name
 
-// Create connection
+$servername = "localhost"; 
+$username = "third"; 
+$password = "third12"; 
+$dbname = "db_php_sql"; 
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
